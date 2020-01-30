ec062a0a7ec8b77b072e396525f7fe39d0b38fe4
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-01-29 20:47:42 -0600

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
 
+def my_collect(arr)
+    new_arr = []
+    i=0
+    while i < arr.length do
+        # if yield(arr[i])
+            new_arr << yield(arr[i])
+        # end
+        i += 1
+    end
+    new_arr
+end
 
